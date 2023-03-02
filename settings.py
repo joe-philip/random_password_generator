f27d007b08901bd2ca129002fc38c94ce2fa00ef
Joe Philip <joe.philip@hotmail.co.in>
2023-03-02 15:12:09 +0530

    modified settings

--- settings.py
+++ settings.py
@@ -1,8 +1,2 @@
-from os import environ
-
-from dotenv import load_dotenv
-
-load_dotenv('.env')
-
-DEBUG = bool(int(environ.get('DEBUG')))
-ORIGINS = environ.get('CORS_ORIGIN_WHITELIST').split(',')
+DEBUG = True
+ORIGINS = '*'
