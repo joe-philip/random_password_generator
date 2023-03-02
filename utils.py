14097b4bfbbfd5e08c1b379f19ee0508392918f5
Joe Philip <joe.philip@hotmail.co.in>
2023-03-02 14:37:51 +0530

    modified success() and fail()

--- utils.py
+++ utils.py
@@ -1,16 +1,26 @@
-def fail(error):
-    response = {
-        'status': False,
-        'message': 'fail',
-        'error': error
-    }
+from flask import jsonify
+
+
+def fail(error, status: int = 400):
+    response = jsonify(
+        {
+            'status': False,
+            'message': 'fail',
+            'error': error
+        }
+    )
+    response.status_code = status
     return response
 
 
-def success(data):
-    response = {
-        'status': True,
-        'message': 'success',
-        'data': data
-    }
+def success(data, status: int = 200):
+    response = jsonify(
+        {
+            'status': True,
+            'message': 'success',
+            'data': data
+        }
+    )
+    response.status_code = status
+    response.headers.add('Access-Control-Allow-Origin', '*')
     return response
